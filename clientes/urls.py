205c9dcb97fe73156e965d50abf9d32d56f28c14
Ticiana CBD <ticiana.cbd@gmail.com>
2026-07-21 20:53:03 +0100

    Adição das funcionalidade de adicionar e editar clientes

--- clientes/urls.py
+++ clientes/urls.py
@@ -2,11 +2,11 @@ from django.urls import path
 
 from . import views
 
-app_name = "marcacoes"
+app_name = "clientes"
 
 urlpatterns = [
-    path("", views.listar, name="cliente"),
-    # path("nova/", views.criar, name="criar"),
-    # path("<int:pk>/editar/", views.editar, name="editar"),
+    path("", views.listar, name="listar"),
+    path("novo/", views.criar, name="criar"),
+    path("<int:pk>/editar/", views.editar, name="editar"),
     # path("<int:pk>/estado/<str:estado>/", views.mudar_estado, name="mudar_estado"),
 ]
